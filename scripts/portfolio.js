d006ac46db91011792d30a5c792c6fe0baa38e3b
tabbytadi <127194636+tabbytadi@users.noreply.github.com>
2025-01-15 20:05:41 +0200

    Update portfolio.js

--- scripts/portfolio.js
+++ scripts/portfolio.js
@@ -39,4 +39,59 @@ document.addEventListener("DOMContentLoaded", () => {
 
   window.addEventListener("scroll", handleScrollAnimation);
   handleScrollAnimation();
+
+  // Typed.js for text animation
+  const typed = new Typed('.multipleText', {
+    strings: ['Frontend Developer', 'Comp Science Student'],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true,
+  });
+
+  // Blurry navbar on scroll
+  window.addEventListener('scroll', function () {
+    const navbarStick = document.querySelector('.navbarSticky');
+    if (window.scrollY > 0) {
+      navbarStick.classList.add('scrolled');
+    } else {
+      navbarStick.classList.remove('scrolled');
+    }
+  });
+
+  // Set active nav link on scroll
+  window.addEventListener('scroll', function () {
+    const sections = document.querySelectorAll('section');
+    const navLinks = document.querySelectorAll('.navbar a');
+    let currentSectionId = '';
+
+    sections.forEach((section) => {
+      const sectionTop = section.offsetTop - 80;
+      const sectionHeight = section.offsetHeight;
+
+      if (window.scrollY >= sectionTop && window.scrollY < sectionTop + sectionHeight) {
+        currentSectionId = section.getAttribute('id');
+      }
+    });
+
+    navLinks.forEach((link) => {
+      if (link.getAttribute('href') === `#${currentSectionId}`) {
+        link.classList.add('active');
+      } else {
+        link.classList.remove('active');
+      }
+    });
+  });
+
+  const dropdownToggles = document.querySelectorAll('.navbar .nav-item.dropdown .toggle');
+
+  dropdownToggles.forEach((toggle) => {
+    toggle.addEventListener('click', function () {
+      const dropdownMenu = toggle.nextElementSibling;
+      const arrow = toggle.querySelector('.arrow');
+
+      dropdownMenu.classList.toggle('show');
+      arrow.classList.toggle('rotate');
+    });
+  });
 });
